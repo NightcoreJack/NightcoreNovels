6202f8f50931a7e548a08e1bff3c26e54c143757
NightcoreJack <32848054+NightcoreJack@users.noreply.github.com>
2023-07-18 20:12:15 -0700

    Update nightcorenovels.lua

--- src/en/nightcorenovels.lua
+++ src/en/nightcorenovels.lua
@@ -1,51 +1,95 @@
--- {"id":0625,"ver":"2.1.1","libVer":"1.0.0","author":"TechnoJo4","dep":["Madara>=2.2.0"]}
+- {"id":7474,"ver":"2.1.1","libVer":"1.0.0","author":"TechnoJo4","dep":["Madara>=2.2.0"]}
 
-return Require("Madara")("https://www.nightcorenovels.com", {
-	id = 0625,
-	name = "Nightcore Novels",
-	imageURL = "https://github.com/nightcorejack/nightcorenovels/raw/dev/icons/nightcorenovels.png",
+return Require("Madara")("https://nightcorenovels.com/", {
 
-	-- defaults values
-	latestNovelSel = "div.col-12.col-md-3.badge-pos-2",
+    id = 7474,
 
-	genres = {
-		"Action",
-		"Adventure",
-		"BG",
-		"BL",
-		"Comedy",
-		"Drama",
-		"Ecchi",
-		"Fantasy",
-		"Gender Bender",
-		"GL",
-		"Harem",
-		"Historical",
-		"Horror",
-		"Isekai",
-		"Josei",
-		"Martial Arts",
-		"Mature",
-		"Mecha",
-		"Modern",
-		"Mystery",
-		"Psychological",
-		"Rebirth",
-		"Romance",
-		"School Life",
-		"Sci-fi",
-		"Seinen",
-		"Shoujo",
-		"Slice of Life",
-		"Supernatural",
-		"Thai Novel",
-		"Tragedy",
-		"Transmigration",
-		"Wuxia",
-		"Xianxia",
-		"Xuanhuan",
-		chinese = "Chinese Novel",
-		japanese = "Japanese Novels",
-		original = "Original Novel",
-	}
+    name = "Nightcore Novels",
+
+    imageURL = "https://nightcorenovels.com/wp-content/uploads/2023/03/434d12f09f54479bd346b9937b32c5f1.png",
+
+    -- defaults values
+
+    latestNovelSel = "div.col-12.col-md-3.badge-pos-2",
+
+    genres = {
+
+        "Action",
+
+        "Adventure",
+
+        "BG",
+
+        "BL",
+
+        "Comedy",
+
+        "Drama",
+
+        "Ecchi",
+
+        "Fantasy",
+
+        "Gender Bender",
+
+        "GL",
+
+        "Harem",
+
+        "Historical",
+
+        "Horror",
+
+        "Isekai",
+
+        "Josei",
+
+        "Martial Arts",
+
+        "Mature",
+
+        "Mecha",
+
+        "Modern",
+
+        "Mystery",
+
+        "Psychological",
+
+        "Rebirth",
+
+        "Romance",
+
+        "School Life",
+
+        "Sci-fi",
+
+        "Seinen",
+
+        "Shoujo",
+
+        "Slice of Life",
+
+        "Supernatural",
+
+        "Thai Novel",
+
+        "Tragedy",
+
+        "Transmigration",
+
+        "Wuxia",
+
+        "Xianxia",
+
+        "Xuanhuan",
+
+        chinese = "Chinese Novel",
+
+        japanese = "Japanese Novels",
+
+        original = "Original Novel",
+
+    }
+
 })
