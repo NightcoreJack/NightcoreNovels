999a99e77b296da94c37cbae3b7a8d14a6418880
NightcoreJack <32848054+NightcoreJack@users.noreply.github.com>
2023-07-19 21:19:07 -0700

    Update nightcorenovels.lua

--- src/en/nightcorenovels.lua
+++ src/en/nightcorenovels.lua
@@ -1,95 +1,13 @@
-- {"id":7474,"ver":"2.1.1","libVer":"1.0.0","author":"TechnoJo4","dep":["Madara>=2.2.0"]}
-
-return Require("Madara")("https://nightcorenovels.com/", {
-
+return Require("Madara")("https://nightcorenovels.com", {
     id = 7474,
-
     name = "Nightcore Novels",
-
     imageURL = "https://nightcorenovels.com/wp-content/uploads/2023/03/434d12f09f54479bd346b9937b32c5f1.png",
 
     -- defaults values
-
-    latestNovelSel = "div.col-12.col-md-3.badge-pos-2",
-
-    genres = {
-
-        "Action",
-
-        "Adventure",
-
-        "BG",
-
-        "BL",
-
-        "Comedy",
-
-        "Drama",
-
-        "Ecchi",
-
-        "Fantasy",
-
-        "Gender Bender",
-
-        "GL",
-
-        "Harem",
-
-        "Historical",
-
-        "Horror",
-
-        "Isekai",
-
-        "Josei",
-
-        "Martial Arts",
-
-        "Mature",
-
-        "Mecha",
-
-        "Modern",
-
-        "Mystery",
-
-        "Psychological",
-
-        "Rebirth",
-
-        "Romance",
-
-        "School Life",
-
-        "Sci-fi",
-
-        "Seinen",
-
-        "Shoujo",
-
-        "Slice of Life",
-
-        "Supernatural",
-
-        "Thai Novel",
-
-        "Tragedy",
-
-        "Transmigration",
-
-        "Wuxia",
-
-        "Xianxia",
-
-        "Xuanhuan",
-
-        chinese = "Chinese Novel",
-
-        japanese = "Japanese Novels",
-
-        original = "Original Novel",
-
-    }
+    latestNovelSel = "div.col-12.col-md-6.badge-pos-2",
+    --searchNovelSel = "div.page-item-detail.text",
+    novelListingURLPath = "",
+    hasSearch = false,
+    genres = {}
 
 })
